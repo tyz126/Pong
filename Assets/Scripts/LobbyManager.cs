0b73d0e8bf4cc076276382454a7a3ad915449aa5
tyz126 <tyz0218@yahoo.com>
2026-03-17 20:31:36 +0800

    Initial commit

---
+++ Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LobbyManager : Photon.PunBehaviour
+{
+    public TMP_InputField inputField;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(inputField.text);
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(inputField.text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel(2);
+    }
+}
