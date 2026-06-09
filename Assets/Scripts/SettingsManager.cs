b7dee48bfbb3aa22bddabee7ca5e361c0de4a5c4
tyz125 <24610@kuencheng.edu.my>
2026-06-09 16:43:12 +0800

    tygetge

---
+++ Assets/Scripts/SettingsManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Linq;
+
+public class SettingsManager : MonoBehaviour
+{
+    public TMP_InputField volumeInputField;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FormatString()
+    {
+        if (!volumeInputField.text.Contains("%") || volumeInputField.text.Count(c =>  c == '%') > 1)
+        {
+            volumeInputField.text = volumeInputField.text.Split('%')[0] + '%';
+        }
+    }
+}
